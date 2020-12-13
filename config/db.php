4ab29c759182bffdac37a9b958e500e2bf41887a
ahmedaminbadji <51519997+ahmedaminbadji@users.noreply.github.com>
2020-12-13 09:21:48 -0800

    Update db.php

--- config/db.php
+++ config/db.php
@@ -1,7 +1,7 @@
 <?php 
     $host ="localhost"; //host
     $user ="root"; //user
-    $pass ="amine07!";  //password
+    $pass ="yourpassword";  //password
     $db = "parkmanager_db"; //db name
 
     $con = mysqli_connect($host,$user,$pass,$db);
